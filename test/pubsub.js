98b64607f7b9b6cb937b6dd390628075970a6876
Brock Whitten <brock@chloi.io>
2013-06-11 00:42:46 -0700

    adds tests

---
+++ test/pubsub.js
@@ -0,0 +1,31 @@
+var should      = require("should")
+var waterfront  = require("../")
+var port        = 9100
+var host        = "localhost"
+var connection  = waterfront.connect(port, host)
+
+describe("pubsub", function(){
+
+  before(function(done){
+    waterfront.listen(port, host, function(){
+      setTimeout(function(){
+        done()
+      }, 25)
+    })
+  })
+
+  it("should be done", function(done){
+    var pub = connection.socket('pub')
+    var sub = connection.socket('sub')
+
+    sub.on("message", function(){
+      done()
+    })
+
+    setTimeout(function(){
+      pub.send("message", { "location": "vancouver"})
+    }, 25)
+
+  })
+
+})
