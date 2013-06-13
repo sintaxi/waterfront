5688f03eb38e5f2c1ff2de195435d18bb25b62d0
Brock Whitten <brock@chloi.io>
2013-06-13 13:12:40 -0700

    tested connection race conditions

--- test/routerdealer.js
+++ test/routerdealer.js
@@ -1,29 +1,76 @@
 var should      = require("should")
 var waterfront  = require("../")
-var port        = 9300
-var host        = "localhost"
-var connection  = waterfront.connect(port, host)
 
 describe("router/dealer", function(){
+  describe("messaging", function(){
+    var port        = 9300
+    var host        = "localhost"
+    var connection  = waterfront.connect(port, host)
 
-  before(function(done){
-    waterfront.listen(port, host, function(){
-      done()
+    before(function(done){
+      waterfront.listen(port, host, function(){
+        done()
+      })
+    })
+
+    it("should be recieve messages", function(done){
+      var req = connection.socket('req')
+      var rep = connection.socket('rep')
+
+      rep.on("message", function(msg, callback){
+        msg.hello = msg.hello.toUpperCase()
+        callback(msg)
+      })
+
+      req.send({ hello: "world" }, function(msg){
+        msg.hello.should.eql("WORLD")
+        done()
+      })
     })
   })
 
-  it("should be recieve messages", function(done){
-    var req = connection.socket('req')
-    var rep = connection.socket('rep')
 
-    rep.on("message", function(msg, callback){
-      msg.hello = msg.hello.toUpperCase()
-      callback(msg)
+
+  describe("connection", function(){
+    it("should be able to send message before broker binds", function(done){
+      var port        = 9400
+      var host        = "localhost"
+      var connection  = waterfront.connect(port, host)
+      var req         = connection.socket('req')
+      var rep         = connection.socket('rep')
+
+      rep.on("message", function(msg, callback){
+        msg.hello = msg.hello.toUpperCase()
+        callback(msg)
+      })
+
+      req.send({ hello: "canada" }, function(msg){
+        msg.hello.should.eql("CANADA")
+        done()
+      })
+
+      waterfront.listen(port, host)
     })
 
-    req.send({ hello: "world" }, function(msg){
-      msg.hello.should.eql("WORLD")
-      done()
+    it("should be able to send request before client connects", function(done){
+      var port        = 9410
+      var host        = "localhost"
+      var connection  = waterfront.connect(port, host)
+      var req         = connection.socket('req')
+
+      req.send({ hello: "brazil" }, function(msg){
+        msg.hello.should.eql("BRAZIL")
+        done()
+      })
+
+      waterfront.listen(port, host, function(){
+        var rep = connection.socket('rep')
+        rep.on("message", function(msg, callback){
+          msg.hello = msg.hello.toUpperCase()
+          callback(msg)
+        })
+      })
+
     })
   })
 
